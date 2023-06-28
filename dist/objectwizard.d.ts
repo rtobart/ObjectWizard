04c50dc84ddd9388ee04091b97b9f018aa07446f
Robinson.tobar <robinson.tobar.ti@gmail.com>
2023-06-27 21:03:24 -0400

    test

--- dist/objectwizard.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'objectwizard/src/index' {
-    export function includesObject(objectsArray: object[], object: object): boolean;
-    export function indexOfObject(objectsArray: object[], object: object): number;
-  }
-  
