df73225fc3ebe3a4ca777792600910d069b67a5b
Robinson.tobar <robinson.tobar.ti@gmail.com>
2023-06-27 20:45:13 -0400

    FIX (TypeScript): update declaration

--- src/objectwizard.d.ts
+++ src/objectwizard.d.ts
@@ -1,4 +1,4 @@
-declare module 'objectwizard' {
+declare module 'objectwizard/src/index' {
     export function includesObject(objectsArray: object[], object: object): boolean;
     export function indexOfObject(objectsArray: object[], object: object): number;
   }
