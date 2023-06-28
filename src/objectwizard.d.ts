e3a445fcc1f597515c37311516237fb797f35eb8
Robinson.tobar <robinson.tobar.ti@gmail.com>
2023-06-27 20:37:31 -0400

    typescript declarations

---
+++ src/objectwizard.d.ts
@@ -0,0 +1,5 @@
+declare module 'objectwizard' {
+    export function includesObject(objectsArray: object[], object: object): boolean;
+    export function indexOfObject(objectsArray: object[], object: object): number;
+  }
+  
